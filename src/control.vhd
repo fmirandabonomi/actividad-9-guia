e266ef4a4a6a32fcec84fe7874b3b0474bdf3e23
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2025-12-04 12:25:38 -0300

    Implementación de l{b|h|w|bu|hu} en unidad de control

--- src/control.vhd
+++ src/control.vhd
@@ -23,7 +23,79 @@ entity control_cpu is
 end control_cpu;
 
 architecture arch of control_cpu is
+    type estado_t is (INICIO, LEE_MEM_PC, CARGA_IR, DECODIFICA , LEE_MEM_DAT_INC_PC, CARGA_RD_DE_MEM);
+    signal estado_sig, estado : estado_t;
 
+    constant OPC_LOAD : std_logic_vector (6 downto 0) := 0x3;
 begin
 
+    registros : process (clk)
+    begin
+        if rising_edge(clk) then
+            if not nreset then
+                estado <= INICIO;
+            else
+                estado <= estado_sig;
+            end if;
+        end if;
+    end process;
+
+    logica_estado_sig : process (all)
+    begin
+        estado_sig <= INICIO;
+        case( estado ) is
+        
+            when INICIO =>
+                estado_sig <= LEE_MEM_PC;
+            when LEE_MEM_PC =>
+                estado_sig <= CARGA_IR;
+            when CARGA_IR =>
+                estado_sig <= DECODIFICA;
+            when DECODIFICA =>
+                case( op ) is
+                    when OPC_LOAD =>
+                        estado_sig <= LEE_MEM_DAT_INC_PC;
+                    when others =>
+                end case; 
+            when LEE_MEM_DAT_INC_PC =>
+                    estado_sig <= CARGA_RD_DE_MEM;
+            when CARGA_RD_DE_MEM =>
+                    estado_sig <= LEE_MEM_PC;
+            when others =>
+        end case ;
+    end process;
+
+    logica_salida : process (all)
+    begin
+        wpc <= '0';
+        wmem <= '0';
+        winst <= '0';
+        wreg <= '0';
+        jump <= '0';
+        s1pc <= '0';
+        alu_mode <= "00";
+        imm_mode <= "000";
+        sel_imm <= '0';
+        data_addr <= '0';
+        mem_source <= '0';
+        case (estado) is
+            when INICIO =>
+                -- por defecto
+            when LEE_MEM_PC =>
+                data_addr <= '0';
+            when CARGA_IR =>
+                winst <= '1';
+            when DECODIFICA =>
+                -- por defecto
+            when LEE_MEM_DAT_INC_PC =>
+                alu_mode <= "00";
+                sel_imm <= '1';
+                data_addr <= '1';
+                wpc <= '1';
+            when CARGA_RD_DE_MEM =>
+                mem_source <= '1';
+                wreg <= '1';
+            when others =>
+        end case;
+    end process;
 end arch ; -- arch
